c39b888c3353a50951e582ff88dcdeee9b74c401
SyfSchydea <sashapwn@gmail.com>
2021-10-25 21:38:01 +0100

    Download mapping data from API

--- main.js
+++ main.js
@@ -1,9 +1,34 @@
 const NOOP = () => {};
 
-function fetchLatest(onLoad, onError=NOOP) {
+// Known values for request are:
+// mapping, latest, 5m, 10m, 30m, 1h, 6h, 24h
+// onLoad should take the parsed data as a parameter
+function fetchApi(request, onLoad, onError=NOOP) {
 	let req = new XMLHttpRequest();
-	req.open("GET", "https://prices.runescape.wiki/api/v1/osrs/latest");
-	req.addEventListener("load", onLoad);
+	req.open("GET", "https://prices.runescape.wiki/api/v1/osrs/" + request);
+	req.addEventListener("load", function() {
+		onLoad(JSON.parse(this.response));
+	});
 	req.addEventListener("error", onError);
 	req.send();
 }
+
+// Cache of /mappings data
+// Contains list of: {
+//   id:       number,
+//   name:     string,
+//   icon:     string,
+//   examine:  string,
+//   members:  bool,
+//   value:    number,
+//   lowalch:  number,
+//   highalch: number,
+//   limit:    number,
+// }
+//
+// Note that the documentation says this is not finalised and could change.
+let mappingData = null;
+
+fetchApi("mapping", data => {
+	mappingData = data;
+});
